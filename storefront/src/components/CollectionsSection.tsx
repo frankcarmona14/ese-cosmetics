0447cb9a9d60a450d6a840ed4678b46f39a5c853
frankcarmona14 <44358240+frankcarmona14@users.noreply.github.com>
2025-03-09 12:28:27 +0000

    Se cambia condición para Metadata

--- storefront/src/components/CollectionsSection.tsx
+++ storefront/src/components/CollectionsSection.tsx
@@ -43,8 +43,8 @@ export const CollectionsSection: React.FC<{ className?: string }> = async ({
           key={collection.id}
         >
           <LocalizedLink href={`/collections/${collection.handle}`}>
-            {collection.metadata.imageUrl &&
-              typeof collection.metadata.imageUrl === "string" && (
+            {typeof collection.metadata?.imageUrl === "string" && 
+              collection.metadata?.description.length > 0 && (
                 <div className="relative mb-4 md:mb-10 w-full aspect-[3/4]">
                   <Image
                     src={collection.metadata.imageUrl}
